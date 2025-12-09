4bf9c17757f2453cf90a46fb0557c977da046350
Shreyas Babar <154340140+Percyakr2161@users.noreply.github.com>
2025-12-09 07:00:13 +0000

    Properties Based In Getter and Setter

---
+++ 10_classes_and_oop/properties_get_set.js
@@ -0,0 +1,26 @@
+function User(email, password){
+    this._email = email;
+    this._password = password
+
+    Object.defineProperty(this, 'email', {
+        get: function(){
+            return this._email.toUpperCase()
+        },
+        set: function(value){
+            this._email = value
+        }
+    })
+    Object.defineProperty(this, 'password', {
+        get: function(){
+            return this._password.toUpperCase()
+        },
+        set: function(value){
+            this._password = value
+        }
+    })
+
+}
+
+const percy = new User("percy@percy.com", "akr")
+
+console.log(percy.email);
